aff44c61082f10ce0feefa13405f38273c800477
FilipeVMartins <filipe_2021@hotmail.com>
2021-04-30 14:18:45 -0300

    redirects

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -45,7 +45,7 @@ export default class Home extends React.Component {
             <div className="App-description-text">
               <p>Here i shall tell a bit more about this project. It's based on Picpay's test but with a few more stuff. Using react to build a SPA where a user 
                 could use it to pay others users from a list, this list should be brought from an API request. The user then could scroll down the list with infinite scrolling 
-                technique, which makes it more mobile friendly rather than clicking paginations while keeping performance on loading, and choose an user to click on its pay button. After that a payment modal would 
+                technique, which makes it more mobile friendly rather than clicking paginations while keeping performance on loading, and choose an user to click on its pay button. After that, a payment modal would 
                 open and then allow the user to input the payment value, choose one of the registered credit cards and click the submit button. When the payment modal is submitted, the 
                 application would start an API post request sending along with it the inputed data and the incoming data from the selected user to be paid, this API would answer
                 confirmation or failure and a response modal would be shown to the user with the respective confirmation answer.</p>
